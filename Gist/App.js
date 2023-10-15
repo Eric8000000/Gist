39188b72e05e4fdd1133f42c75de2f59ef0f9f72
nikhil <nikhildevs.108@gmail.com>
2023-10-15 00:48:24 -0700

    Fonts!!!

--- Gist/App.js
+++ Gist/App.js
@@ -1,8 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
-import List from './components/List.js';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+import List from "./components/List.js";
+import React, { useState, useEffect } from "react";
 import {
   Dimensions,
   StyleSheet,
@@ -16,8 +15,25 @@ export default function App() {
   console.log("App Excecuted");
   const handlePress = () => console.log("Text Press");
 
+  //states for lists
+  //add lists, delete lists
+  //when list is clicked, expended is true
   const [expanded, setExpanded] = useState(true);
   const [active, setActive] = useState(true);
+
+  //loads the font
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        "Kadwa-Regular": require("./assets/fonts/Kadwa-Regular.ttf"),
+        "Kalam-Light": require("./assets/fonts/Kalam-Light.ttf"),
+      });
+
+      //Text.defaultProps.style.fontFamily = "custom-font";
+    }
+
+    loadFont();
+  }, []);
 
   return (
     <SafeAreaView
@@ -31,17 +47,19 @@ export default function App() {
         //HEADER
         style={styles.header}
       >
-        <Text style={{ fontSize: 45 }}>Gist.</Text>
+        <Text style={{ fontSize: 45, fontFamily: "Kadwa-Regular" }}>Gist.</Text>
       </View>
 
       <View
         //MAIN SCREEN
         style={styles.main}
-      > 
-        <List active={active} 
-          expanded={expanded} 
-          setExpanded={setExpanded} 
-          setActive={setActive} />
+      >
+        <List
+          active={active}
+          expanded={expanded}
+          setExpanded={setExpanded}
+          setActive={setActive}
+        />
       </View>
 
       <View
