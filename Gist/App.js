6976c5ee64ba3b47089c64a18e9d8e4dbdef8736
nikhil <nikhildevs.108@gmail.com>
2023-10-14 23:43:40 -0700

    Main screen created in App js

--- Gist/App.js
+++ Gist/App.js
@@ -1,16 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+} from "react-native";
 
 export default function App() {
-
   console.log("App Excecuted");
   const handlePress = () => console.log("Text Press");
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text onPress = {handlePress}>Gist</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView
+      style={{
+        backgroundColor: "#F9BFC6",
+        flex: 1,
+      }}
+    >
+      <StatusBar> </StatusBar>
+      <View
+        //HEADER
+        style={{
+          backgroundColor: "#AA767C",
+          flex: 0.125,
+          justifyContent: "center",
+        }}
+      >
+        <Text style={{ fontSize: 45 }}>Gist.</Text>
+      </View>
+
+      <View
+        //MAIN SCREEN
+        style={{
+          backgroundColor: "#F9BFC6",
+          flex: 1,
+        }}
+      ></View>
+
+      <View
+        //FOOTER
+        style={{
+          backgroundColor: "#F9BFC6",
+          flex: 0.15,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            alignItems: "Center",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            title="Add New List"
+            onPress={() => console.log("Bruh")}
+            color="#AA767C"
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -18,12 +71,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F9BFC6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  Text : {
-      flex: 1,
-      fontFamily: "comic-sans",
-  }
 });
