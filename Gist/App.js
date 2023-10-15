5c93b42c2ed6ed60bd1b8a863bdfa925fe7cf861
nikhil <nikhildevs.108@gmail.com>
2023-10-14 22:10:03 -0700

    First Changes

--- Gist/App.js
+++ Gist/App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
 
 export default function App() {
+
+  console.log("App Excecuted");
+  const handlePress = () => console.log("Text Press");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress = {handlePress}>Gist</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F9BFC6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text : {
+      flex: 1,
+      fontFamily: "comic-sans",
+  }
 });
